e7399cc2a0fdcebba51b45a48a35424757bb89d1
PerdomoPer <gustavo.perdomo@estudante.ifms.edu.br>
2023-11-01 07:31:36 -0400

    aaaaa

--- src/aplicacao/salva-filme.use-case.test.ts
+++ src/aplicacao/salva-filme.use-case.test.ts
@@ -1,17 +1,19 @@
-import { describe, test, expect } from "vitest";
+import {describe, test , expect} from 'vitest'
 import SalvaFilme from './salva-filme.use-case'
-
-describe("testando usecase de salvar filme",()=>{
-    test("deve salvar um filme",()=>{
+describe("Testando usecase de salvar filme",()=>{
+    test("Deve salvar um filme",()=>{
+        //Entrada
         const filme = {
-            id: 1,
-            titulo: "test",
-            descricao: "test",
-            foto: "test"
+            id:1,
+            titulo:"test",
+            descricao:"test",
+            foto:"test"
         }
-
+        //Processamento
         const salvaFilme = new SalvaFilme()
-        const resultado = salvaFilme.execute(filme)
+        const {id, titulo, descricao, foto} = filme
+        const resultado = salvaFilme.execute({id,titulo,descricao,foto})
+        //Resultado
         expect(resultado).toEqual(filme)
     })
 })
--- src/aplicacao/salva-filme.use-case.ts
+++ src/aplicacao/salva-filme.use-case.ts
@@ -1,15 +1,16 @@
 import BancoEmMemoria from "../infra/banco/banco-em-memoria"
-
 class SalvaFilme{
     private banco = new BancoEmMemoria()
     constructor(){}
-    public execute(input:Input):Output | undefined{
-        const{id,titulo,descricao,foto} = input
+    public execute(input:Input):Output|undefined{
+        const {id, titulo, descricao, foto} = input
+        //Salvar no Banco
         const resultado = this.banco.salvar({id,titulo,descricao,foto})
-        if(!resultado) return undefined
-        return {id,titulo,descricao,foto}
-    }}
-
+        //Retornar o resultado
+        if(!resultado) return undefined 
+        return {id, titulo, descricao, foto}
+    }
+}
 export default SalvaFilme
 
 type Input = {
@@ -18,7 +19,6 @@ type Input = {
     descricao:string,
     foto:string
 }
-
 type Output = {
     id:number,
     titulo:string,
